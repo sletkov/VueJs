4f7e02b506b76be93972716091008cf1f36ded3f
Semen <sletkovsemen@gmail.com>
2021-01-28 15:38:04 +0300

    title&info added

---
+++ script.js
@@ -0,0 +1,13 @@
+var title = new Vue ({
+    el:'#title',
+    data:{
+        title: "Список студентов"
+    }
+})
+
+var info = new Vue({
+    el:'#info',
+    data: {
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et esse neque? Expedita natus cupiditate id fuga? Aliquam culpa quo ipsam similique sint incidunt in! Corporis officiis culpa sint sequi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et esse neque? Expedita natus cupiditate id fuga? Aliquam culpa quo ipsam similique sint incidunt in! Corporis officiis culpa sint sequi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et esse neque? Expedita natus cupiditate id fuga? Aliquam culpa quo ipsam similique sint incidunt in! Corporis officiis culpa sint sequi."
+    }
+})
