c13e0261f9c7fa9ab1852211c93d09be247df282
Semen <sletkovsemen@gmail.com>
2021-01-28 16:16:05 +0300

    form with length count added

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ var info = new Vue({
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et esse neque? Expedita natus cupiditate id fuga? Aliquam culpa quo ipsam similique sint incidunt in! Corporis officiis culpa sint sequi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et esse neque? Expedita natus cupiditate id fuga? Aliquam culpa quo ipsam similique sint incidunt in! Corporis officiis culpa sint sequi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et esse neque? Expedita natus cupiditate id fuga? Aliquam culpa quo ipsam similique sint incidunt in! Corporis officiis culpa sint sequi."
     }
 })
+
+var form = new Vue({
+    el:"#form",
+    data: {
+        formTitle: "Форма",
+        surname: "Фамилия",
+        name:"Имя",
+        phrase1:'',
+        phrase2:''  
+    },
+    computed: {
+        length: function () {
+        return (this.phrase1.length + this.phrase2.length)
+        }
+    }
+});
